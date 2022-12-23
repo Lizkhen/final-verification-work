ea45e3d3f11c9d0b5af7090507773efcbfaa75a5
Lizkhen <slezadrakona@gmail.com>
2022-12-23 12:05:45 +0300

    Solution

---
+++ Solution/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите элементы вашего массива через пробел");
+string [] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+string [] newArray = new string [COUNT(array)];
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine ("Новый массив из элементов, длина которых меньше, либо равна 3 символам:");
+Console.WriteLine($"[{string.Join(", ", NewArray(array))}]");
+
+int COUNT (string [] array )
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (array[i].Length <=3)
+        { 
+            count++;
+        }
+    }
+    return count;
+}
+
+string [] NewArray (string [] array)
+{
+    for (int i =0, j=0; i<array.Length;i++)
+    {
+        if (array[i].Length <=3)
+        { 
+            newArray[j] = array[i]; 
+            j++;
+        }
+    } 
+    return (newArray);
+}
